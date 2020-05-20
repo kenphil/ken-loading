db06ce46641cbe653f434d4f133ad9a9010c74c3
kenphil <kenphil@vip.qq.com>
2020-05-20 12:00:57 +0800

    initial commit

---
+++ js/wave.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  let container = document.getElementById('wave2')
+  let bars = container.children
+  
+  let delayArr = [0, 0.1, 0.2, 0.3, 0.4]
+
+  for (let i = 0, len = bars.length; i < len; i++) {
+    let index = parseInt(Math.random() * delayArr.length)
+    bars[i].style.animationDelay = delayArr[index] + 's'
+    delayArr.splice(index, 1)
+  }
+}
